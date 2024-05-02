4d12b7eca1ec2d230150d38bb4304ddac3121a0c
Sufian Ahmed <sufianahmedch22@gmail.com>
2024-05-02 19:59:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffers from "./SpecialOffers";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  CustomerReviews,
+  SpecialOffers,
+  Footer,
+  Subscribe,
+  SuperQuality,
+};
